4dd3e7d6ba573a1fd6f8f876f38992f4d1ff8483
Yi Joon Choi <choi990406@gmail.com>
2021-01-05 00:08:46 +0900

    210105

--- app/src/main/java/com/example/androidpracticekt/MainActivity.kt
+++ app/src/main/java/com/example/androidpracticekt/MainActivity.kt
@@ -5,10 +5,15 @@ import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
+    override fun onCreate(savedInstanceState: Bundle?) { //액티비티가 최초 실행되면 이곳을 수행한다.
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        tv_title.setText("Hello World!")
+        btn_getText.setOnClickListener {
+            //에딧텍스트에 입력되어있는 값을 가지고 와서 텍스트뷰에 뿌려준다.
+            var resultText = et_id.text.toString() //에딧텍스트에 입력되어있는값.
+            tv_result.setText(resultText) //입력된 값을 텍스트뷰에 set 해줌.
+        }
+
     }
 }
